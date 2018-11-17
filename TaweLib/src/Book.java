9f55c9f70cb09475d6099d04510cee52b5d743df
Owner <Owner@137.44.87.36>
2018-11-17 16:19:20 +0000

    Constructors, setters and getters for Resource and subclasses.

--- TaweLib/src/Book.java
+++ TaweLib/src/Book.java
@@ -1,4 +1,61 @@
+import javafx.scene.image.Image;
 
-public class Book {
+public class Book extends Resource {
+	
+	private String author;
+	private String genre;
+	private String publisher;
+	private int ISBN;
+	private String language;
+	
+	public Book(String author, String genre, String publisher, int ISBN, String language, int ID, String title, int year, Image thumbnailImage, int numOfCopies) {
+		super(ID, title, year, thumbnailImage, numOfCopies);
+		this.author = author;
+		this.genre = genre;
+		this.publisher = publisher;
+		this.ISBN = ISBN;
+		this.language = language;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public int getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(int iSBN) {
+		ISBN = iSBN;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	
 	
 }
--- TaweLib/src/Resource.java
+++ TaweLib/src/Resource.java
@@ -1,6 +1,59 @@
+import javafx.scene.image.Image;
 
 public class Resource {
-	public Resource() {
+	
+	protected int ID;
+	protected String title;
+	protected int year;
+	protected Image thumbnailImage;
+	protected int numOfCopies;
+	
+	public Resource(int ID, String title, int year, Image thumbnailImage, int numOfCopies) {
+		this.ID = ID;
+		this.title = title;
+		this.year = year;
+		this.thumbnailImage = thumbnailImage;
+		this.numOfCopies = numOfCopies;
 		
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public Image getThumbnailImage() {
+		return thumbnailImage;
+	}
+
+	public void setThumbnailImage(Image thumbnailImage) {
+		this.thumbnailImage = thumbnailImage;
+	}
+
+	public int getNumOfCopies() {
+		return numOfCopies;
+	}
+
+	public void setNumOfCopies(int numOfCopies) {
+		this.numOfCopies = numOfCopies;
 	}
 }
