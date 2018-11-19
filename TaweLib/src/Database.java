660b83ab7f646a87688971d5611631829253b2cd
Oliver Nixon <oliver.nixon1@gmail.com>
2018-11-19 15:10:09 +0000

    Defining important methods

--- TaweLib/src/Database.java
+++ TaweLib/src/Database.java
@@ -3,11 +3,41 @@ import java.sql.*;
 public class Database {
 	
 	//static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://sql134.main-hosting.eu/u591224331_lib";
-	static final String DB_USERNAME = "u591224331_grp41";
-	static final String DB_PASS = "group41";
+	private static final String DB_URL = "jdbc:mysql://sql134.main-hosting.eu/u591224331_lib";
+	private static final String DB_USERNAME = "u591224331_grp41";
+	private static final String DB_PASS = "group41";
 	
-	public static void main(String args[]){
+	private Connection database;
+	
+	Database() {
+		this.database = connectDatabase();
+	}
+	
+	private Connection connectDatabase() {
+		Connection newConnection = null;
+		try {
+			newConnection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASS);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return newConnection;
+	}
+	
+	//Method to check database table exists and is functioning properly
+	private boolean checkDatabase() {
+		
+		return true;
+	}
+	
+	private void createDatabase() {
+		
+	}
+	
+	
+	
+	
+	/*public static void main(String args[]){
 		
 		Connection conn = null;
 		Statement stmt = null;
@@ -54,5 +84,5 @@ public class Database {
 		   }//end try
 		   System.out.println("Goodbye!");
 		
-	}
+	}*/
 }
