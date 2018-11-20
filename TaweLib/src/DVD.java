bc8c5f02c5c21bbf9c178de9f185622d985572a5
Bobby <Bobby@DESKTOP-3BAAESQ.mshome.net>
2018-11-20 14:58:56 +0000

    Javadoc finished on the entire Resource Subsystem hierarchy and javadoc regenerated.

--- TaweLib/src/DVD.java
+++ TaweLib/src/DVD.java
@@ -1,13 +1,38 @@
+
+/**
+ * Class that creates a DVD object
+ * @author Boris Andreev and Kyriacos Mosphilis
+ */
 import javafx.scene.image.Image;
 
 public class DVD extends Resource {
-	
+
 	private String director;
 	private int runtime;
 	private String language;
 	private String subtitles;
 
-	public DVD( String title, int year, Image thumbnailImage, int numOfCopies, String director, int runtime,
+	/**
+	 * Constructor for DVD
+	 * 
+	 * @param title
+	 *            Title of the DVD
+	 * @param year
+	 *            Year of release
+	 * @param thumbnailImage
+	 *            Cover Image
+	 * @param numOfCopies
+	 *            How many are available in the library
+	 * @param director
+	 *            Director of the film
+	 * @param runtime
+	 *            Runtime of film
+	 * @param language
+	 *            Language of the film
+	 * @param subtitles
+	 *            Subtitles available for the film
+	 */
+	public DVD(String title, int year, Image thumbnailImage, int numOfCopies, String director, int runtime,
 			String language, String subtitles) {
 		super(title, year, thumbnailImage, numOfCopies);
 		this.director = director;
@@ -16,34 +41,48 @@ public class DVD extends Resource {
 		this.subtitles = subtitles;
 	}
 
+	/**
+	 * Getter method for film director
+	 * 
+	 * @return Name of film director
+	 */
 	public String getDirector() {
 		return director;
 	}
 
-	public void setDirector(String director) {
-		this.director = director;
-	}
-
+	/**
+	 * Getter method for film runtime
+	 * 
+	 * @return The runtime of the film
+	 */
 	public int getRuntime() {
 		return runtime;
 	}
 
-	public void setRuntime(int runtime) {
-		this.runtime = runtime;
-	}
-
+	/**
+	 * Getter method for the film's language
+	 * 
+	 * @return The language of the film
+	 */
 	public String getLanguage() {
 		return language;
 	}
 
-	public void setLanguage(String language) {
-		this.language = language;
-	}
-
+	/**
+	 * Getter method for film's list of subtitles
+	 * 
+	 * @return Subtitles available for the film
+	 */
 	public String getSubtitles() {
 		return subtitles;
 	}
 
+	/**
+	 * Setter method for the film's subtitles
+	 * 
+	 * @param subtitles
+	 *            String that holds the available subtitles
+	 */
 	public void setSubtitles(String subtitles) {
 		this.subtitles = subtitles;
 	}
--- TaweLib/src/LaptopComputer.java
+++ TaweLib/src/LaptopComputer.java
@@ -1,42 +1,63 @@
+
+/**
+ * Class that creates a new Laptop computer object
+ * @author Boris Andreev and Kyriacos Mosphilis
+ * @version 1.00
+ */
 import javafx.scene.image.Image;
 
 public class LaptopComputer extends Resource {
-	
+
 	private String manufacturer;
 	private String model;
 	private String OS;
-	
+
+	/**
+	 * Constructor for laptop object
+	 * 
+	 * @param title
+	 *            Name of computer
+	 * @param year
+	 *            Year of manufacture
+	 * @param thumbnailImage
+	 *            Picture of the laptop
+	 * @param numOfCopies
+	 *            Total available computers in the library
+	 * @param manufacturer
+	 *            Manufacturer of the computer
+	 * @param model
+	 *            Model of the computer
+	 * @param OS
+	 *            Operating system running on the laptop
+	 */
 	public LaptopComputer(String title, int year, Image thumbnailImage, int numOfCopies, String manufacturer,
-			String model, String oS) {
+			String model, String OS) {
 		super(title, year, thumbnailImage, numOfCopies);
 		this.manufacturer = manufacturer;
 		this.model = model;
-		OS = oS;
+		this.OS = OS;
 	}
 
+	/**
+	 * Getter method for the manufacturer of the laptop
+	 * 
+	 * @return Manufacturer of the laptop
+	 */
 	public String getManufacturer() {
 		return manufacturer;
 	}
-
-	public void setManufacturer(String manufacturer) {
-		this.manufacturer = manufacturer;
-	}
-
+	/**
+	 * Getter method for the model of the laptop
+	 * @return The model of the laptop 
+	 */
 	public String getModel() {
 		return model;
 	}
-
-	public void setModel(String model) {
-		this.model = model;
-	}
-
+	/**
+	 * Getter method for laptop OS
+	 * @return Operating system running on the laptop
+	 */
 	public String getOS() {
 		return OS;
 	}
-
-	public void setOS(String oS) {
-		OS = oS;
-	}
-	
-
 }
