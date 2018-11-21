0ed8cf9f6cfa98d4469e39ca1986a9dd5209328e
Kyriacos <Owner@DESKTOP-M2NK5PJ.lan>
2018-11-21 19:38:44 +0000

    Minor bug fixes in SearchBrowse and some testing in ResourceMain.

--- TaweLib/src/SearchBrowse.java
+++ TaweLib/src/SearchBrowse.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class SearchBrowse {
 	
-	private static ArrayList<Resource> resourceList; //Call within the Librarian class, each time he creates a new resource.
+	private static ArrayList<Resource> resourceList = new ArrayList<>(); //Call within the Librarian class, each time he creates a new resource.
 	
 	public static void addResource(Resource r) {
 		resourceList.add(r);
@@ -12,54 +12,54 @@ public class SearchBrowse {
 		return resourceList;
 	}
 	
-	public static ArrayList<Resource> getLaptops() {
-		ArrayList<Resource> laptopList = new ArrayList<>();
-		int count = 1;
-		do {
-			if (resourceList.get(count) instanceof LaptopComputer) {
-				laptopList.add(resourceList.get(count));
-			}
-			count++;
-		} while (count == resourceList.size());
-		
-		return laptopList;
-	}
-	
 	public static ArrayList<Resource> getBooks() {
 		ArrayList<Resource> bookList = new ArrayList<>();
-		int count = 1;
+		int count = 0;
 		do {
 			if (resourceList.get(count) instanceof Book) {
 				bookList.add(resourceList.get(count));
 			}
 			count++;
-		} while (count == resourceList.size());
+		} while (count != resourceList.size());
 		
 		return bookList;
 	}
 	
 	public static ArrayList<Resource> getDVDs() {
 		ArrayList<Resource> dvdList = new ArrayList<>();
-		int count = 1;
+		int count = 0;
 		do {
 			if (resourceList.get(count) instanceof DVD) {
 				dvdList.add(resourceList.get(count));
 			}
 			count++;
-		} while (count == resourceList.size());
+		} while (count != resourceList.size());
 		
 		return dvdList;
 	}
 	
+	public static ArrayList<Resource> getLaptops() {
+		ArrayList<Resource> laptopList = new ArrayList<>();
+		int count = 0;
+		do {
+			if (resourceList.get(count) instanceof LaptopComputer) {
+				laptopList.add(resourceList.get(count));
+			}
+			count++;
+		} while (count != resourceList.size());
+		
+		return laptopList;
+	}
+	
 	public static ArrayList<Resource> search(String keyword) {
 		ArrayList<Resource> list = new ArrayList<>();
-		int count = 1;
+		int count = 0;
 		do {
 			if (resourceList.get(count).getTitle() == keyword) /* need to be changed to something sensible */{
 				list.add(resourceList.get(count));
 			}
 			count++;
-		} while (count == resourceList.size());
+		} while (count != resourceList.size());
 		
 		return list;
 	}
