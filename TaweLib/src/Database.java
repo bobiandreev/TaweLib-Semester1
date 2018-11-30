40d3600d26e9249589a1927c99f793eac78834b4
Oliver Nixon <oliver.nixon1@gmail.com>
2018-11-30 13:24:45 +0000

    rip db

--- TaweLib/src/Database.java
+++ TaweLib/src/Database.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class Database {
 	
-	//static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
 	private static final String DB_URL = "jdbc:mysql://sql134.main-hosting.eu/u591224331_lib";
 	private static final String DB_USERNAME = "u591224331_grp41";
 	private static final String DB_PASS = "group41";
@@ -91,55 +91,4 @@ public class Database {
 		}
 		newDB.closeConnection();
 	}
-	
-	
-	
-	/*public static void main(String args[]){
-		
-		Connection conn = null;
-		Statement stmt = null;
-		
-		try {
-			
-			System.out.println("Connecting to database...");
-			conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASS);
-			
-			System.out.println("Creating statement...");
-			stmt = conn.createStatement();
-			String sql;
-			sql = "SELECT * FROM test";
-			ResultSet rs = stmt.executeQuery(sql);
-			
-			while(rs.next()){
-				String name = rs.getString("Name");
-				
-				System.out.println(name);
-			}
-			
-			rs.close();
-			stmt.close();
-			conn.close();
-		}catch(SQLException se){
-		      //Handle errors for JDBC
-		      se.printStackTrace();
-		   }catch(Exception e){
-		      //Handle errors for Class.forName
-		      e.printStackTrace();
-		   }finally{
-		      //finally block used to close resources
-		      try{
-		         if(stmt!=null)
-		            stmt.close();
-		      }catch(SQLException se2){
-		      }// nothing we can do
-		      try{
-		         if(conn!=null)
-		            conn.close();
-		      }catch(SQLException se){
-		         se.printStackTrace();
-		      }//end finally try
-		   }//end try
-		   System.out.println("Goodbye!");
-		
-	}*/
 }
