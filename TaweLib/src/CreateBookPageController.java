8c75f5601d2462f2d008583e11db5d7ebd16bc69
Bobby <Bobby@DESKTOP-3BAAESQ.mshome.net>
2018-12-03 11:55:40 +0000

    Made it so that book can be created with the gui.

--- TaweLib/src/CreateBookPageController.java
+++ TaweLib/src/CreateBookPageController.java
@@ -15,7 +15,7 @@ public class CreateBookPageController {
     private BorderPane createBookPane;
 
     @FXML
-    private TextField itemID;
+    private TextField itemID; // dont need
 
     @FXML
     private TextField title;
@@ -31,6 +31,9 @@ public class CreateBookPageController {
 
     @FXML
     private TextField publisher;
+    
+    // need to add genre
+    // need to add number of copies
 
     @FXML
     private void clickOnBack(ActionEvent event) {
@@ -39,7 +42,12 @@ public class CreateBookPageController {
 
     @FXML
     private void clickOnConfirm(ActionEvent event) {
-
+    	String title = this.title.getText();
+    	String author = this.author.getText();
+    	String publisher = this.publisher.getText();
+    	int year = Integer.parseInt(this.year.getText());
+    	Book newBook = new Book(title, year, null, 5, author, publisher);
+    	SearchBrowse.addResource(newBook);
     }
     
     @FXML
