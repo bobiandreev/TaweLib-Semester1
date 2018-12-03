6e1e57291b86acfbb7be649c4d1f87f6ea47cc45
ming <ming@192.168.128.41>
2018-12-03 22:45:48 +0000

    d

--- TaweLib/src/BorrowRequestPageController.java
+++ TaweLib/src/BorrowRequestPageController.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -8,18 +7,28 @@ import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+/**
+ * The BorrowRequestPageController Class which controls the BorrowRequestPage.fxml
+ * @author
+ *
+ */
 public class BorrowRequestPageController {
 	private ArrayList<Copy> borrowRequestCopies = new ArrayList<>();
 
-	@FXML
-	private BorderPane borrowRequestPane;
+	@FXML private BorderPane borrowRequestPane;
 
-	@FXML
-	private ListView<String> borrowRequestList;
+	@FXML private ListView<String> borrowRequestList;
 
 	// private ArrayList<String> borrowRequests = new ArrayList<>();
 	// private ArrayList<> borrowRequests = new ArrayList<>();
-
+	
+	/**
+	 * Initialize the controller.
+	 * The following happen in this order:
+	 * 1) First an instance of the controller is created (the constructor is called),
+	 * 2) Next the @FXML variables are bound to the GUI components.
+	 * 3) Finally, this initialize method is called.
+	 */
 	public void initialize() {
 		for (User user : Librarian.getUsersList()) {
 			for (Copy copy : user.getRequestedItems()) {
@@ -30,9 +39,13 @@ public class BorrowRequestPageController {
 			}
 		}
 	}
-
-	@FXML
-	void clickOnApprove(ActionEvent event) {
+	
+	/**
+	 * Actions will be made when the user click on the button
+	 * @param event
+	 */
+	@FXML void clickOnApprove(ActionEvent event) {
+		
 		int selectedIndex = borrowRequestList.getSelectionModel().getSelectedIndex();
 		
 		if (selectedIndex < 0) {
@@ -43,8 +56,7 @@ public class BorrowRequestPageController {
 			alert.showAndWait();
 			return;
 		}
-		
-		
+
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Info");
 		alert.setHeaderText(null);
@@ -52,22 +64,26 @@ public class BorrowRequestPageController {
 		alert.showAndWait();
 		
 		Copy currentCopy = borrowRequestCopies.get(selectedIndex);
-		
-		
-		
+			
 		boolean yes = true;
 		Librarian.approveBorrow(yes,currentCopy);
 		borrowRequestList.getItems().remove(selectedIndex);
 		borrowRequestCopies.remove(selectedIndex);
 	}
 
-	@FXML
-	void clickOnBack(ActionEvent event) {
+	/**
+	 * Actions will be made when the user click on the button
+	 * @param event
+	 */
+	@FXML void clickOnBack(ActionEvent event) {
 		closeWindow();
 	}
 
-	@FXML
-	void clickOnNotApprove(ActionEvent event) {
+	/**
+	 * Actions will be made when the user click on the button
+	 * @param event
+	 */
+	@FXML void clickOnNotApprove(ActionEvent event) {
 		int selectedIndex = borrowRequestList.getSelectionModel().getSelectedIndex();
 		
 		if (selectedIndex < 0) {
@@ -78,7 +94,6 @@ public class BorrowRequestPageController {
 			alert.showAndWait();
 			return;
 		}
-		
 		
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Info");
@@ -93,9 +108,11 @@ public class BorrowRequestPageController {
 		borrowRequestList.getItems().remove(selectedIndex);
 		borrowRequestCopies.remove(selectedIndex);
 	}
-
-	@FXML
-	private void closeWindow() { // A method which close the window
+	
+	/**
+	* Close the window.
+	*/
+	@FXML private void closeWindow() { // A method which close the window
 		Stage stage = (Stage) borrowRequestPane.getScene().getWindow();
 		stage.close();
 	}
