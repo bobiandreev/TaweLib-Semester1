c66b046c2d332f9e0ee9ce6947e48b74d85a7e26
Oliver Nixon <oliver.nixon1@gmail.com>
2018-12-08 14:42:19 +0000

    javadocumnetational

--- TaweLib/src/UserRequestedItemsPageController.java
+++ TaweLib/src/UserRequestedItemsPageController.java
@@ -6,9 +6,12 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
+ * <h1> User Requested Items Page Controller for GUI. </h1> 
  * The UserRequestedItemsPageController Class which controls the UserRequestedItemsPage.fxml
+ * 
  * @author Ming
- *
+ * @version 1.0
+ * @since 2018-12-05
  */
 public class UserRequestedItemsPageController {
 
@@ -19,7 +22,11 @@ public class UserRequestedItemsPageController {
     private ListView<String> requestedItemsList;
     
     private User curUser;
-    
+ 
+    /**
+     * This method handles the requested item of the User and displays the appropriate
+     * message accordingly.
+     */
     public void initialize() {
     	curUser = LoginController.getLoggedUser();
     	for(Copy copy: curUser.getRequestedItems()) {
@@ -30,11 +37,21 @@ public class UserRequestedItemsPageController {
     		requestedItemsList.getItems().add(copyProperties);
     	}
     }
-
+    
+    /**
+	 * Actions will be made when the user clicks on the button.
+	 * Returns to previous window by closing the current/most recent one.
+	 * 
+	 * @param event
+	 */
     @FXML
     void clickOnBack(ActionEvent event) {
     	closeWindow();
     }
+    
+    /**
+	 * This method closes the window.
+	 */
     private void closeWindow() { // A method which close the window
 		Stage stage = (Stage) requestedItemsPane.getScene().getWindow();
 	    stage.close();
