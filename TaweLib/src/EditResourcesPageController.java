555bfb59238c5b1f30778781bfd55e48fe67bee2
Oliver Nixon <oliver.nixon1@gmail.com>
2018-12-08 14:16:20 +0000

    jdoc

--- TaweLib/src/EditResourcesPageController.java
+++ TaweLib/src/EditResourcesPageController.java
@@ -5,9 +5,12 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
+ * <h1> Make changes in regards to the resources available. </h1>
  * The EditResourcesPageController Class which controls the EditResourcesPage.fxml
+ * 
  * @author Ming
- *
+ * @version 1.0
+ * @since 2018-12-03
  */
 public class EditResourcesPageController {
 
@@ -17,16 +20,31 @@ public class EditResourcesPageController {
     @FXML
     private TextField changeQuantity;
     
+    /**
+	 * Actions will be made when the user clicks on the button.
+	 * Confirms the changes made to the resource(s).
+	 * 
+	 * @param event - When the mouse clicks on the button.
+	 */
     @FXML
     private void clickOnConfirm(ActionEvent event) {
 
     }
 
+    /**
+	 * Actions will be made when the user clicks on the button.
+	 * Returns to previous window by closing the current/most recent one.
+	 * 
+	 * @param event - When the mouse clicks on the button.
+	 */
     @FXML
     private void clickOnBack(ActionEvent event) {
     	closeWindow();
     }
 
+    /**
+	 * This method closes the window.
+	 */
     @FXML
     private void closeWindow() { // A method which close the window
 		Stage stage = (Stage) editResourcesPane.getScene().getWindow();
